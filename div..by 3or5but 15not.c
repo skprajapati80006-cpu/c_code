1cee65d746f20d1b9d613cb96aa0bb6f6e2f7837
shubham kumar prajapati <skprajapati80006@gmail.com>
2026-09-11 17:59:13 +0530

    Update div..by 3or5but 15not.c

--- div..by 3or5but 15not.c
+++ div..by 3or5but 15not.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     int n;
-    printf("enter a number");
+    printf("enter a number : ");
     scanf("%d",&n);
     if(n%5==0  ||  n%3==0){
         if(n%3!=15 || n%5!=0);
